be8068d496ae97a58d2d0adc57104cfed6e87f84
kornelius-stack <kornelius@gmail.com>
2026-04-09 10:51:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="DNNK Klimamonitor Proxy")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
